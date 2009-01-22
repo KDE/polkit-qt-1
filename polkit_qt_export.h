c49be8330cf133b049a4ca14ade149dc85d63b41
Lukas Appelhans <l.appelhans@gmx.de>
2009-01-22 21:42:59 +0000

    Forgot a file, sorry
    svn path=/trunk/extragear/base/PolicyKit-kde/polkit-qt/; revision=915364

---
+++ polkit_qt_export.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Dario Freddi                                    *
+ *   drf54321@yahoo.it                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
+#ifndef SHAMAN_MACROS_H
+#define SHAMAN_MACROS_H
+
+/** \file shaman_macros.h
+    \brief Contains Macros used in Shaman
+    
+    This contains some macros that help in exporting/importing symbols and plugins
+*/
+
+#define POLKIT_QT_EXPORT __attribute__ ((visibility("default")))
+
+#endif /*SHAMAN_MACROS_H*/

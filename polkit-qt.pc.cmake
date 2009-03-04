e463d01b6d3b9fe80bd64718bef84ef60a99b94d
Dario Freddi <drf@kde.org>
2009-03-04 10:39:06 +0000

    Improving the build system, including pkgconfig file
    svn path=/trunk/kdesupport/polkit-qt/; revision=934960

---
+++ polkit-qt.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@LIB_INSTALL_DIR@
+includedir=@CMAKE_INSTALL_PREFIX@/include
+
+Name: Polkit-qt
+Description: Convenience library for using PolicyKit with a Qt-styled API
+Version: @POLKIT_QT_VERSION_STRING@
+Requires: QtCore polkit polkit-dbus polkit-grant
+Libs: -L${libdir} -lpolkit-qt
+Cflags: -I${includedir}

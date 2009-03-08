b31d38ec47ae9b4e11ff6418e3c1dfb1ebb8ea20
Dario Freddi <drf@kde.org>
2009-03-08 09:43:14 +0000

    Pkgconfig support for splitted library
    svn path=/trunk/kdesupport/polkit-qt/; revision=936654

--- polkit-qt-core.pc.cmake
+++ polkit-qt-core.pc.cmake
@@ -4,8 +4,8 @@ libdir=@LIB_INSTALL_DIR@
 includedir=@CMAKE_INSTALL_PREFIX@/include
 
 Name: Polkit-qt
-Description: Convenience library for using PolicyKit with a Qt-styled API
+Description: Convenience library for using PolicyKit with a Qt-styled API, non-GUI functions
 Version: @POLKIT_QT_VERSION_STRING@
-Requires: QtCore QtGui polkit polkit-dbus polkit-grant
-Libs: -L${libdir} -lpolkit-qt-core -lpolkit-qt-gui
+Requires: QtCore polkit polkit-dbus polkit-grant
+Libs: -L${libdir} -lpolkit-qt-core
 Cflags: -I${includedir}

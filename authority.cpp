05c53deaf3fb1b3c1451dbed982172c0fe5d3058
Jaroslav Řezník <jreznik@redhat.com>
2009-09-16 11:08:12 +0000

    ConsoleKit code rewritten for consoleKitDBChanged signal Patch by Radek Novacek
    svn path=/branches/work/polkit-qt-1/; revision=1024307

--- authority.cpp
+++ authority.cpp
@@ -69,6 +69,8 @@ public:
 
     void init();
     void dbusFilter(const QDBusMessage &message);
+    void dbusSignalAdd(const QString &service, const QString &path, const QString &interface, const QString &name);
+    void seatSignalsConnect(const QString &seat);
 
     Authority *q;
     PolkitAuthority *pkAuthority;
@@ -77,9 +79,6 @@ public:
     QDBusConnection *m_systemBus;
 
     static void pk_config_changed();
-
-    QDomDocument introspect(const QString &service, const QString &path, const QDBusConnection &c);
-    QStringList getSignals(const QDomDocument &iface);
 };
 
 Authority::Authority(PolkitAuthority *authority, QObject *parent)
@@ -123,109 +122,60 @@ void Authority::Private::init()
     // TODO: test it first!
     g_signal_connect(G_OBJECT(pkAuthority), "changed", G_CALLBACK(pk_config_changed), NULL);
     
-
-    /*
     // need to listen to NameOwnerChanged
-    if (QDBusConnection::systemBus().connect(DBUS_SERVICE_DBUS, QString(), DBUS_INTERFACE_DBUS, "NameOwnerChanged",
-            Authority::instance(), SLOT(dbusFilter(const QDBusMessage &)))) {
-    } else {
-        qWarning() << "Could not connect to the service bus to listen to NameOwnerChanged";
+    dbusSignalAdd("org.freedesktop.DBus", "/", "org.freedesktop.DBus", "NameOwnerChanged");
+
+    QString consoleKitService("org.freedesktop.ConsoleKit");
+    QString consoleKitManagerPath("/org/freedesktop/ConsoleKit/Manager");
+    QString consoleKitManagerInterface("org.freedesktop.ConsoleKit.Manager");
+    QString consoleKitSeatInterface("org.freedesktop.ConsoleKit.Seat");
+
+    // first, add signals SeadAdded and SeatRemoved from ConsoleKit Manager
+    dbusSignalAdd(consoleKitService, consoleKitManagerPath, consoleKitManagerInterface, "SeatAdded");
+    dbusSignalAdd(consoleKitService, consoleKitManagerPath, consoleKitManagerInterface, "SeatRemoved");
+
+    // then we need to extract all seats from ConsoleKit
+    QDBusMessage msg = QDBusMessage::createMethodCall(consoleKitService, consoleKitManagerPath, consoleKitManagerInterface, "GetSeats");
+    msg = QDBusConnection::systemBus().call(msg);
+    // this method returns a list with present seats
+    QList<QString> seats;
+    qVariantValue<QDBusArgument> (msg.arguments()[0]) >> seats;
+    // it can be multiple seats present so connect all their signals
+    foreach (QString seat, seats)
+    {
+        seatSignalsConnect(seat);
     }
+}
 
-    // Ok, let's get what we need here
-    QStringList sigs;
+void Authority::Private::seatSignalsConnect(const QString &seat)
+{
+    QString consoleKitService("org.freedesktop.ConsoleKit");
+    QString consoleKitSeatInterface("org.freedesktop.ConsoleKit.Seat");
+    // we want to connect to all slots of the seat
+    dbusSignalAdd(consoleKitService, seat, consoleKitSeatInterface, "DeviceAdded");
+    dbusSignalAdd(consoleKitService, seat, consoleKitSeatInterface, "DeviceRemoved");
+    dbusSignalAdd(consoleKitService, seat, consoleKitSeatInterface, "SessionAdded");
+    dbusSignalAdd(consoleKitService, seat, consoleKitSeatInterface, "SessionRemoved");
+    dbusSignalAdd(consoleKitService, seat, consoleKitSeatInterface, "ActiveSessionChanged");
+}
 
-    sigs += getSignals(introspect("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Manager", QDBusConnection::systemBus()));
-    sigs += getSignals(introspect("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Session1", QDBusConnection::systemBus()));
-    sigs += getSignals(introspect("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Seat1", QDBusConnection::systemBus()));
-
-    foreach(const QString &sig, sigs) {
-
-        if (!QDBusConnection::systemBus().connect("org.freedesktop.ConsoleKit",
-                QString(),
-                QString(),
-                sig,
-                Authority::instance(),
-                SLOT(dbusFilter(const QDBusMessage &)))) {
-            qWarning() << "Could not connect to the service bus to listen "
-            "to the following signals:"
-            << sigs;
-        }
-
-    }
-*/
-    /*if (dbus_error_is_set(&dbus_error)) {
-        dbus_error_free(&dbus_error);
-        qWarning() << "Failed to initialize ConsoleKit";
-        m_hasError  = true;
-        return;
-    }*/
-
-    m_lastError.clear();
-    m_hasError = false;
+void Authority::Private::dbusSignalAdd(const QString &service, const QString &path, const QString &interface, const QString &name)
+{
+    QDBusConnection::systemBus().connect(service, path, interface, name,
+            Authority::instance(), SLOT(dbusFilter(const QDBusMessage &)));
 }
 
 void Authority::Private::dbusFilter(const QDBusMessage &message)
 {
-    // forward only NameOwnerChanged and ConsoleKit signals to PolkitTracker
-    /*if ((message.type() == QDBusMessage::SignalMessage &&
-            message.interface() == "org.freedesktop.DBus" &&
-            message.member()    == "NameOwnerChanged")
-            ||
-            (!message.interface().isEmpty() &&
-             message.interface().startsWith(QLatin1String("org.freedesktop.ConsoleKit")))) {
+    if (message.type() == QDBusMessage::SignalMessage)
+    {
+        qDebug() << "INCOMING SIGNAL:" << message.member();
+        emit q->consoleKitDBChanged();
 
-        DBusMessage *msg = 0;
-        DBusMessageIter args;
-        msg = dbus_message_new_signal(
-                  message.path().toUtf8().data(), message.interface().toUtf8().data(),
-                  message.member().toUtf8().data());
-        dbus_message_iter_init_append(msg, &args);
-        foreach(QVariant arg, message.arguments()) {
-            char *argument = qstrdup(arg.toString().toAscii());
-            int numarg;
-
-            switch (arg.type()) {
-            case QVariant::String:
-                if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &argument)) {
-                    qFatal("Out Of Memory!");
-                    exit(1);
-                }
-                break;
-            case QVariant::Bool:
-                if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_BOOLEAN, argument)) {
-                    qFatal("Out Of Memory!");
-                    exit(1);
-                }
-                break;
-            case QVariant::Int:
-                numarg = arg.toInt();
-                if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_INT32, &numarg)) {
-                    qFatal("Out Of Memory!");
-                    exit(1);
-                }
-                break;
-
-                // Don't try to understand this part, it's too weird for me too
-            case QVariant::UserType:
-                if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT_PATH, &argument)) {
-                    qFatal("Out Of Memory!");
-                    exit(1);
-                }
-                break;
-            default:
-                qWarning() << "Type not handled in the switch. It should be a " << arg.type();
-                break;
-            }
-
-
-        }
-
-        if (msg) {
-            emit q->consoleKitDBChanged();
-        }
-
-    }*/
+        // TODO: Test this with the multiseat support
+        if (message.member() == "SeatAdded")
+            seatSignalsConnect(qVariantValue<QDBusObjectPath> (message.arguments()[0]).path());
+    }
 }
 
 bool Authority::hasError() const
@@ -246,60 +196,6 @@ QString Authority::lastError() const
 void Authority::Private::pk_config_changed()
 {
     emit Authority::instance()->configChanged();
-}
-
-QDomDocument Authority::Private::introspect(const QString &service, const QString &path, const QDBusConnection &c)
-{
-    QDomDocument doc;
-
-    QDBusInterface iface(service, path, "org.freedesktop.DBus.Introspectable", c);
-    if (!iface.isValid()) {
-        QDBusError err(iface.lastError());
-        qWarning() << QString("Cannot introspect object %1 at %2:\n  %3 (%4)\n").arg(path).arg(
-            service).arg(err.name()).arg(err.message());
-        return doc;
-    }
-
-    QDBusReply<QString> xml = iface.call("Introspect");
-
-    if (!xml.isValid()) {
-        QDBusError err(xml.error());
-        if (err.isValid()) {
-            qWarning() << QString("Call to object %1 at %2:\n  %3 (%4) failed\n").arg(
-                path).arg(service).arg(err.name()).arg(err.message());
-        } else {
-            qWarning() << QString("Invalid XML received from object %1 at %2\n").arg(
-                path).arg(service);
-        }
-        return doc;
-    }
-
-    doc.setContent(xml);
-    return doc;
-}
-
-QStringList Authority::Private::getSignals(const QDomDocument &doc)
-{
-    QStringList retlist;
-
-    QDomElement node = doc.documentElement();
-    QDomElement child = node.firstChildElement();
-
-    while (!child.isNull()) {
-        if (child.tagName() == QLatin1String("node") || child.tagName() == QLatin1String("interface")) {
-            QDomElement iface = child.firstChildElement();
-            while (!iface.isNull()) {
-                if (iface.tagName() == QLatin1String("signal")) {
-                    retlist.append(iface.attribute("name"));
-                }
-                iface = iface.nextSiblingElement();
-            }
-        }
-
-        child = child.nextSiblingElement();
-    }
-
-    return retlist;
 }
 
 PolkitAuthority *Authority::getPolkitAuthority() const

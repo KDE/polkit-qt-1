95585755a623c68848bb3584599ca78b4ec333fd
Jaroslav Řezník <jreznik@redhat.com>
2009-09-16 15:06:09 +0000

    Subject class encapsuling PolkitSubject
    svn path=/branches/work/polkit-qt-1/; revision=1024409

---
+++ subject.cpp
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the Polkit-qt project
+ * Copyright (C) 2009 Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "subject.h"
+
+#include <polkit/polkit.h>
+#include <glib-object.h>
+
+using namespace PolkitQt;
+
+Subject::Subject(QObject * parent)
+    : QObject(parent), m_subject(0)
+{
+    g_type_init();    
+}
+
+Subject::~Subject()
+{
+    g_object_unref(m_subject);
+}
+
+PolkitSubject * Subject::subject()
+{
+    return m_subject;
+}
+
+QString Subject::toString() const
+{
+    Q_ASSERT(m_subject);
+    return QString::fromUtf8(polkit_subject_to_string(m_subject));
+}
+
+Subject * Subject::fromString(const QString & string)
+{
+    m_subject = polkit_subject_from_string(string.toUtf8().data(), NULL);
+    return this;
+}
+
+UnixProcess::UnixProcess(qint64 pid, QObject * parent) : Subject(parent)
+{
+    m_subject = polkit_unix_process_new(pid);
+}
+
+UnixProcess::UnixProcess(qint64 pid, quint64 startTime, QObject * parent) : Subject(parent)
+{
+    m_subject = polkit_unix_process_new_full(pid, startTime);
+}
+
+qint64 UnixProcess::pid() const
+{
+    return polkit_unix_process_get_pid((PolkitUnixProcess *) m_subject);
+}
+
+qint64 UnixProcess::startTime() const
+{
+    return polkit_unix_process_get_start_time((PolkitUnixProcess *) m_subject);
+}
+
+void UnixProcess::setPid(qint64 pid)
+{
+    polkit_unix_process_set_pid((PolkitUnixProcess *) m_subject, pid);
+}
+
+// ----- SystemBusName
+SystemBusName::SystemBusName(const QString &name, QObject * parent) : Subject(parent)
+{
+    m_subject = polkit_system_bus_name_new(name.toUtf8().data());
+}
+
+QString SystemBusName::name() const
+{
+    return QString::fromUtf8(polkit_system_bus_name_get_name((PolkitSystemBusName *) m_subject));
+}
+    
+void SystemBusName::setName(const QString &name)
+{
+    polkit_system_bus_name_set_name((PolkitSystemBusName *) m_subject, name.toUtf8().data());
+}
+
+// ----- SystemSession
+UnixSession::UnixSession(const QString &sessionId, QObject * parent) : Subject(parent)
+{
+    m_subject = polkit_unix_session_new(sessionId.toUtf8().data());
+}
+
+QString UnixSession::sessionId() const
+{
+    return QString::fromUtf8(polkit_unix_session_get_session_id((PolkitUnixSession *) m_subject));
+}
+
+void UnixSession::setSessionId(const QString &sessionId)
+{
+    polkit_unix_session_set_session_id((PolkitUnixSession *) m_subject, sessionId.toUtf8().data());
+}
---
+++ subject.h
@@ -0,0 +1,191 @@
+/*
+ * This file is part of the PolKit1-qt project
+ * Copyright (C) 2009 Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef POLICYKIT_QT_SUBJECT_H
+#define POLICYKIT_QT_SUBJECT_H
+
+#include "export.h"
+
+#include <QtCore/QObject>
+#include <polkit/polkit.h>
+
+/**
+ * \namespace PolkitQt PolkitQt
+ *
+ * \brief Namespace wrapping PolicyKit-Qt classes
+ *
+ * This namespace wraps all PolicyKit-Qt classes.
+ */
+namespace PolkitQt
+{
+
+/**
+ * \class Subject subject.h Subject
+ * \author Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * \brief This class represents PolicyKit subjects 
+ *
+ * This class encapsulates the PolkitSubject interface.
+ * 
+ * \see UnixProcess
+ * \see SystemBusName
+ * \see UnixSession
+ */    
+class POLKIT_QT_EXPORT Subject : public QObject
+{
+    Q_OBJECT
+public:
+    ~Subject();
+    QString toString() const;
+    Subject * fromString(const QString & string);
+    PolkitSubject * subject();
+protected:
+    PolkitSubject * m_subject;
+    explicit Subject(QObject *parent = 0);
+};
+
+/**
+ * \class UnixProcess subject.h UnixProcess
+ * \author Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * \brief A class for representing a UNIX process. 
+ *
+ * To uniquely identify processes, both the process
+ * id and the start time of the process (a monotonic 
+ * increasing value representing the time since the 
+ * kernel was started) is used.
+ * 
+ */    
+class POLKIT_QT_EXPORT UnixProcess: public Subject
+{
+    Q_OBJECT;
+public:
+    /**
+    * Subject constructor, takes one parameter - PID. The start time
+    * of process will be looked automaticaly.
+    *
+    * \param pid An Unix process PID.
+    */    
+    UnixProcess(qint64 pid, QObject *parent = 0);
+
+    /**
+    * Subject constructor, takes two parameters - PID and start time.
+    *
+    * \param pid An Unix process PID.
+    * \param startTime An Unix process start time.
+    */    
+    UnixProcess(qint64 pid, quint64 startTime, QObject *parent = 0);
+    
+    /**
+    * Returns Unix process PID.
+    *
+    * \return A PID of associated Unix process.
+    */    
+    qint64 pid() const;
+
+    /**
+    * Returns Unix process start time.
+    *
+    * \return A start time of associated Unix process.
+    */    
+    qint64 startTime() const;
+    
+    /**
+    * Sets Unix process PID.
+    *
+    * \param pid An Unix process PID.
+    */
+    void setPid(qint64 pid);
+};
+
+/**
+ * \class SystemBusName subject.h UnixProcess
+ * \author Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * \brief A class for representing a process owning a unique name on the system bus. 
+ */    
+class POLKIT_QT_EXPORT SystemBusName: public Subject
+{
+    Q_OBJECT;
+public:
+    /**
+    * Subject constructor, takes one parameter - system bus name.
+    *
+    * \param name A unique system bus name.
+    */    
+    SystemBusName(const QString &name, QObject *parent = 0);
+
+    /**
+    * Returns system bus name.
+    *
+    * \return A unique system bus name.
+    */    
+    QString name() const;
+    
+    /**
+    * Sets system bus name.
+    *
+    * \param name System bus name.
+    */
+    void setName(const QString &name);
+    
+    // TODO: unixProcess
+};
+
+/**
+ * \class UnixSession subject.h UnixProcess
+ * \author Jaroslav Reznik <jreznik@redhat.com>
+ *
+ * \brief A class for representing unix session.
+ *
+ * The session id is an opaque string obtained from 
+ * ConsoleKit.
+ */    
+class POLKIT_QT_EXPORT UnixSession: public Subject
+{
+    Q_OBJECT;
+public:
+    /**
+    * Subject constructor, takes one parameter - session id.
+    *
+    * \param sessionId The session id.
+    */    
+    UnixSession(const QString &sessionId, QObject *parent = 0);
+
+    /**
+    * Returns session id.
+    *
+    * \return A session id.
+    */    
+    QString sessionId() const;
+    
+    /**
+    * Sets session id.
+    *
+    * \param name A session id.
+    */
+    void setSessionId(const QString &sessionId);
+    
+    // TODO: constructor from PID?
+};
+
+}
+
+#endif

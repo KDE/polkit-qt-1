3e538103be6aa744f2124ce51fc6f0fe495716ec
Radek Novacek <rnovacek@redhat.com>
2009-10-06 12:43:30 +0000

    Added constructor from PolkitDetails to the Details class and added GPL licence to details.cpp|h
    svn path=/branches/work/polkit-qt-1/; revision=1031912

--- details.cpp
+++ details.cpp
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the Polkit-qt project
+ * Copyright (C) 2009 Dario Freddi <drf54321@gmail.com>
+ * Copyright (C) 2007 David Faure <faure@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include "details.h"
 
 #include <QtCore/QDebug>
@@ -9,6 +30,12 @@ Details::Details(QObject *parent) : QObject(parent)
 {
     g_type_init();
     m_polkitDetails = polkit_details_new();
+}
+
+Details::Details(PolkitDetails *pkDetails) : QObject(NULL)
+{
+    g_type_init();
+    m_polkitDetails = pkDetails;
 }
 
 Details::~Details()
--- details.h
+++ details.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the Polkit-qt project
+ * Copyright (C) 2009 Dario Freddi <drf54321@gmail.com>
+ * Copyright (C) 2007 David Faure <faure@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #ifndef DETAILS_H
 #define DETAILS_H
 
@@ -29,6 +50,7 @@ public:
      * Creates a new Details object
      */
     Details(QObject *parent = NULL);
+    Details(PolkitDetails *pkDetails);
 
     ~Details();
 

3388b8126b840a7b9097beb8730b8e68a7b2429a
Jaroslav Řezník <jreznik@redhat.com>
2009-10-13 12:56:24 +0000

    Removes cyclic dependency
    svn path=/branches/work/polkit-qt-1/; revision=1034759

--- polkit-qt-agent-1.pc.cmake
+++ polkit-qt-agent-1.pc.cmake
@@ -6,6 +6,6 @@ includedir=@CMAKE_INSTALL_PREFIX@/include
 Name: Polkit-qt-1
 Description: Convenience library for using PolicyKit Agent with a Qt-styled API
 Version: @POLKIT_QT_VERSION_STRING@
-Requires: QtCore QtGui polkit-qt-agent-1
+Requires: QtCore QtGui
 Libs: -L${libdir} -lpolkit-qt-agent-1
 Cflags: -I${includedir}

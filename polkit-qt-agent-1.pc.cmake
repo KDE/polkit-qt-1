02575e08543a3ff63ff712eeafcf0f2ee51272f4
Dario Freddi <drf@kde.org>
2009-10-15 08:11:06 +0000

    Fix pkgconfig files
    Signed-off-by: Dario Freddi <drf@kde.org>
    
    svn path=/branches/work/polkit-qt-1/; revision=1035483

--- polkit-qt-agent-1.pc.cmake
+++ polkit-qt-agent-1.pc.cmake
@@ -3,8 +3,8 @@ exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@LIB_INSTALL_DIR@
 includedir=@CMAKE_INSTALL_PREFIX@/include
 
-Name: Polkit-qt-1
-Description: Convenience library for using PolicyKit Agent with a Qt-styled API
+Name: polkit-qt-agent-1
+Description: Convenience library for using polkit Agent with a Qt-styled API
 Version: @POLKIT_QT_VERSION_STRING@
 Requires: QtCore QtGui
 Libs: -L${libdir} -lpolkit-qt-agent-1
--- polkit-qt-gui-1.pc.cmake
+++ polkit-qt-gui-1.pc.cmake
@@ -3,8 +3,8 @@ exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@LIB_INSTALL_DIR@
 includedir=@CMAKE_INSTALL_PREFIX@/include
 
-Name: Polkit-qt-1
-Description: Convenience library for using PolicyKit with a Qt-styled API, GUI functions
+Name: polkit-qt-gui-1
+Description: Convenience library for using polkit with a Qt-styled API, GUI classes
 Version: @POLKIT_QT_VERSION_STRING@
 Requires: QtCore QtGui polkit-qt-core-1
 Libs: -L${libdir} -lpolkit-qt-gui-1

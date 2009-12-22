a11ddf4c34cab582b23bd4cb1d944e9a22fc9a42
Dario Freddi <drf@kde.org>
2009-12-22 16:49:47 +0000

    Change to -1 in polkitqtversion
    Signed-off-by: Dario Freddi <drf@kde.org>
    
    svn path=/trunk/kdesupport/polkit-qt-1/; revision=1065153

--- polkitqt1version.h.cmake
+++ polkitqt1version.h.cmake
@@ -23,82 +23,82 @@
 
 #include "export.h"
 
-/// @brief PolkitQt version as string at compile time.
-#define POLKITQT1_VERSION_STRING "${CMAKE_POLKITQT-1_VERSION_STRING}"
+/// @brief PolkitQt-1 version as string at compile time.
+#define POLKITQT-1_VERSION_STRING "${CMAKE_POLKITQT-1_VERSION_STRING}"
 
-/// @brief The major PolkitQt version number at compile time
-#define POLKITQT1_VERSION_MAJOR ${CMAKE_MAJOR_POLKITQT-1_VERSION}
+/// @brief The major PolkitQt-1 version number at compile time
+#define POLKITQT-1_VERSION_MAJOR ${CMAKE_MAJOR_POLKITQT-1_VERSION}
 
-/// @brief The minor PolkitQt version number at compile time
-#define POLKITQT1_VERSION_MINOR ${CMAKE_MINOR_POLKITQT-1_VERSION}
+/// @brief The minor PolkitQt-1 version number at compile time
+#define POLKITQT-1_VERSION_MINOR ${CMAKE_MINOR_POLKITQT-1_VERSION}
 
-/// @brief The PolkitQt release version number at compile time
-#define POLKITQT1_VERSION_RELEASE ${CMAKE_RELEASE_POLKITQT-1_VERSION}
+/// @brief The PolkitQt-1 release version number at compile time
+#define POLKITQT-1_VERSION_RELEASE ${CMAKE_RELEASE_POLKITQT-1_VERSION}
 
 /**
- * \brief Create a unique number from the major, minor and release number of a %PolkitQt version
+ * \brief Create a unique number from the major, minor and release number of a %PolkitQt-1 version
  *
  * This function can be used for preprocessing. For version information at runtime
- * use the version methods in the PolkitQt namespace.
+ * use the version methods in the PolkitQt-1 namespace.
  */
-#define POLKITQT1_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
+#define POLKITQT-1_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
 /**
- * \brief %PolkitQt Version as a unique number at compile time
+ * \brief %PolkitQt-1 Version as a unique number at compile time
  *
- * This macro calculates the %PolkitQt version into a number. It is mainly used
+ * This macro calculates the %PolkitQt-1 version into a number. It is mainly used
  * through POLKITQT1_IS_VERSION in preprocessing. For version information at runtime
- * use the version methods in the PolkitQt namespace.
+ * use the version methods in the PolkitQt-1 namespace.
  */
-#define POLKITQT1_VERSION \
-    POLKITQT1_MAKE_VERSION(POLKITQT1_VERSION_MAJOR,POLKITQT1_VERSION_MINOR,POLKITQT1_VERSION_RELEASE)
+#define POLKITQT-1_VERSION \
+    POLKITQT-1_MAKE_VERSION(POLKITQT-1_VERSION_MAJOR,POLKITQT-1_VERSION_MINOR,POLKITQT-1_VERSION_RELEASE)
 
 /**
- * \brief Check if the %PolkitQt version matches a certain version or is higher
+ * \brief Check if the %PolkitQt-1 version matches a certain version or is higher
  *
  * This macro is typically used to compile conditionally a part of code:
  * \code
  * #if POLKITQT1_IS_VERSION(2,1)
- * // Code for PolkitQt 2.1
+ * // Code for PolkitQt-1 2.1
  * #else
- * // Code for PolkitQt 2.0
+ * // Code for PolkitQt-1 2.0
  * #endif
  * \endcode
  *
  * For version information at runtime
- * use the version methods in the PolkitQt namespace.
+ * use the version methods in the PolkitQt-1 namespace.
  */
-#define POLKITQT1_IS_VERSION(a,b,c) ( POLKITQT1_VERSION >= POLKITQT1_MAKE_VERSION(a,b,c) )
+#define POLKITQT-1_IS_VERSION(a,b,c) ( POLKITQT-1_VERSION >= POLKITQT-1_MAKE_VERSION(a,b,c) )
 
 
 namespace PolkitQt1 {
     /**
-     * @brief Returns the major number of PolkitQt's version, e.g.
-     * 1 for %PolkitQt 1.0.2.
+     * @brief Returns the major number of PolkitQt-1's version, e.g.
+     * 1 for %PolkitQt-1 1.0.2.
      * @return the major version number at runtime.
      */
     POLKITQT1_EXPORT unsigned int versionMajor();
 
     /**
-     * @brief Returns the minor number of PolkitQt's version, e.g.
-     * 0 for %PolkitQt 1.0.2.
+     * @brief Returns the minor number of PolkitQt-1's version, e.g.
+     * 0 for %PolkitQt-1 1.0.2.
      * @return the minor version number at runtime.
      */
     POLKITQT1_EXPORT unsigned int versionMinor();
 
     /**
-     * @brief Returns the release of PolkitQt's version, e.g.
-     * 2 for %PolkitQt 1.0.2.
+     * @brief Returns the release of PolkitQt-1's version, e.g.
+     * 2 for %PolkitQt-1 1.0.2.
      * @return the release number at runtime.
      */
     POLKITQT1_EXPORT unsigned int versionRelease();
 
     /**
-     * @brief Returns the %PolkitQt version as string, e.g. "1.0.2".
+     * @brief Returns the %PolkitQt-1 version as string, e.g. "1.0.2".
      *
      * On contrary to the macro POLKITQT1_VERSION_STRING this function returns
-     * the version number of PolkitQt at runtime.
-     * @return the %PolkitQt version. You can keep the string forever
+     * the version number of PolkitQt-1 at runtime.
+     * @return the %PolkitQt-1 version. You can keep the string forever
      */
     POLKITQT1_EXPORT const char* versionString();
 }

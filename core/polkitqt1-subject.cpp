18866459f2fe7ab1c72b55e5603eed5443fae6c7
Raymond Wooninck <tittiatcoke@gmail.com>
2014-03-25 20:23:01 +0100

    Replace deprecated polkit methods
    REVIEW: 117056

--- core/polkitqt1-subject.cpp
+++ core/polkitqt1-subject.cpp
@@ -122,13 +122,13 @@ Subject Subject::fromString(const QString &string)
 UnixProcessSubject::UnixProcessSubject(qint64 pid)
         : Subject()
 {
-    setSubject(polkit_unix_process_new(pid));
+    setSubject(polkit_unix_process_new_for_owner(pid, 0, getuid ()));
 }
 
 UnixProcessSubject::UnixProcessSubject(qint64 pid, quint64 startTime)
         : Subject()
 {
-    setSubject(polkit_unix_process_new_full(pid, startTime));
+    setSubject(polkit_unix_process_new_for_owner(pid, startTime, getuid ()));
 }
 
 UnixProcessSubject::UnixProcessSubject(PolkitUnixProcess *pkUnixProcess)

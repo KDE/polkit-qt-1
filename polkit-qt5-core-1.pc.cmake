1f151d81381106c15084280f1e11209d259ae45c
John Zimmermann <me@johnnynator.dev>
2021-06-19 15:47:56 +0200

    Change installed .cmake and .pc files to contain realtive paths
    The LIB_DESTINATION and INCLUDE_DESTIONATION variables do hold little
    value, since one can just override the normal CMAKE variables as needed.
    Furthermore using CMAKE_INSTALL_FULL_LIBDIR is harmful, since this
    prevents CMake to use it's logic to install relocatable .cmake files.
    Also libdir and includedir in pkgconfig files need to be prefixed by
    either ${exec_prefix} or ${prefix} for the same relocation reasons.

--- polkit-qt5-core-1.pc.cmake
+++ polkit-qt5-core-1.pc.cmake
@@ -1,7 +1,7 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
-libdir=@LIB_DESTINATION@
-includedir=@INCLUDE_DESTINATION@/@POLKITQT-1_INCLUDE_PATH@
+libdir=${exec_prefix}/@CMAKE_INSTALL_LIBDIR@
+includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@/@POLKITQT-1_INCLUDE_PATH@
 
 Name: @POLKITQT-1_CORE_PCNAME@
 Description: Convenience library for using polkit with a Qt-styled API, non-GUI classes

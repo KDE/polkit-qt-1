44097bad6f77031dd58a5a0930690e1a4d77d663
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-06-19 12:46:57 +0200

    Drop PolkitQt1::version*() API declaration, never implemented
    Seems the version header once was inspired by kdelibs'
    kdecore/util/kdeversion.h.cmake but missed out the cpp counterpart.
    
    Given no-one has ever complained about the missing runtime-version check
    option, just dropping it then.

--- polkitqt1-version.h.cmake
+++ polkitqt1-version.h.cmake
@@ -8,8 +8,6 @@
 #ifndef _POLKITQT1_VERSION_H_
 #define _POLKITQT1_VERSION_H_
 
-#include "polkitqt1-core-export.h"
-
 /// @brief PolkitQt-1 version as string at compile time.
 #define POLKITQT1_VERSION_STRING "${POLKITQT-1_VERSION_STRING}"
 
@@ -25,8 +23,7 @@
 /**
  * \brief Create a unique number from the major, minor and release number of a %PolkitQt-1 version
  *
- * This function can be used for preprocessing. For version information at runtime
- * use the version methods in the PolkitQt-1 namespace.
+ * This function can be used for preprocessing.
  */
 #define POLKITQT1_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
@@ -34,8 +31,7 @@
  * \brief %PolkitQt-1 Version as a unique number at compile time
  *
  * This macro calculates the %PolkitQt-1 version into a number. It is mainly used
- * through POLKITQT1_IS_VERSION in preprocessing. For version information at runtime
- * use the version methods in the PolkitQt-1 namespace.
+ * through POLKITQT1_IS_VERSION in preprocessing.
  */
 #define POLKITQT1_VERSION \
     POLKITQT1_MAKE_VERSION(POLKITQT1_VERSION_MAJOR,POLKITQT1_VERSION_MINOR,POLKITQT1_VERSION_PATCH)
@@ -51,43 +47,7 @@
  * // Code for PolkitQt-1 2.0
  * #endif
  * \endcode
- *
- * For version information at runtime
- * use the version methods in the PolkitQt-1 namespace.
  */
 #define POLKITQT1_IS_VERSION(a,b,c) ( POLKITQT1_VERSION >= POLKITQT1_MAKE_VERSION(a,b,c) )
-
-
-namespace PolkitQt1 {
-    /**
-     * @brief Returns the major number of PolkitQt-1's version, e.g.
-     * 1 for %PolkitQt-1 1.0.2.
-     * @return the major version number at runtime.
-     */
-    POLKITQT1_CORE_EXPORT unsigned int versionMajor();
-
-    /**
-     * @brief Returns the minor number of PolkitQt-1's version, e.g.
-     * 0 for %PolkitQt-1 1.0.2.
-     * @return the minor version number at runtime.
-     */
-    POLKITQT1_CORE_EXPORT unsigned int versionMinor();
-
-    /**
-     * @brief Returns the patch number of PolkitQt-1's version, e.g.
-     * 2 for %PolkitQt-1 1.0.2.
-     * @return the release number at runtime.
-     */
-    POLKITQT1_CORE_EXPORT unsigned int versionPatch();
-
-    /**
-     * @brief Returns the %PolkitQt-1 version as string, e.g. "1.0.2".
-     *
-     * On contrary to the macro POLKITQT1_VERSION_STRING this function returns
-     * the version number of PolkitQt-1 at runtime.
-     * @return the %PolkitQt-1 version. You can keep the string forever
-     */
-    POLKITQT1_CORE_EXPORT const char* versionString();
-}
 
 #endif
